01e41a79ceb1c8bd512ee203c9b8fa7a18b305fb
joonida <joon123121@hotmail.com>
2023-03-23 00:07:50 -0600

    update files

--- public/login.js
+++ public/login.js
@@ -1,6 +1,5 @@
-function login()
-{
-    const nameEl = document.querySelector("#name");
-    localStorage.setItem("userName", nameEl.value);
-    window.location.href = "play.html";
+function login() {
+  const nameEl = document.querySelector("#name");
+  localStorage.setItem("userName", nameEl.value);
+  window.location.href = "play.html";
 }
